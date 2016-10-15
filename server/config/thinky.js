3a9d423a2541aac93d0235e3ef7dfa96cc0bb126
SirSavary <sirsavary@outlook.com>
2016-10-15 12:20:41 -0400

    Fix production RethinkDB settings

--- server/config/thinky.js
+++ server/config/thinky.js
@@ -1,8 +1,20 @@
-const thinky = require('thinky')({
-    // host: "aws-us-east-1-portal.23.dblayer.com",
-    // port: "15962",
-    // authKey: "usDyOGWH4AHORIVXZTlA0H5ijjmacpK72PqrnvY0",
-    db: "development"
-});
+const env = process.env.NODE_ENV || 'dev';
+const rethinkUrl = process.env.RETHINK_URL || "admin:@localhost:28015";
 
-module.exports = thinky;
+let options = {
+    user: rethinkUrl.split('@')[0].split(':')[0],
+    password: rethinkUrl.split('@')[0].split(':')[1],
+    host: rethinkUrl.split('@')[1].split(':')[0],
+    port: rethinkUrl.split('@')[1].split(':')[1],
+    db: env,
+};
+
+if (env !== 'dev') {
+    const FS = require('fs');
+    let caCert = FS.readFileSync(__dirname + "/../../compose.io.cacert.pem");
+    options.ssl = {
+        ca: caCert
+    };
+}
+
+module.exports = require('thinky')(options);
